e89ffefb1603a9764638b8b67083011aca722c2a
seoyeon615 <bsy070615@duksung.ac.kr>
2026-05-29 23:46:06 +0900

    구현 끝

--- toy_project/toy_project/TestBack/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Course, Test
-
-admin.site.register(Course)
-admin.site.register(Test)
-
--- toy_project/toy_project/TestBack/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestbackConfig(AppConfig):
-    name = 'TestBack'
